0796f184acb77c7352b6cee43336b1970b46af2e
houmengyi <hmy1095543542@163.com>
2019-05-17 18:07:20 +0800

    时间选择

--- src/components/page/children/popupOpenTime.vue
+++ src/components/page/children/popupOpenTime.vue
@@ -1,8 +1,45 @@
 <template>
   <div class="popupOpenTime">
 		<div class="data-content">
-        <p>Select a duration to hold contacts within the Journey</p>
+      <p>Select a duration to hold contacts within the Journey</p>
+      <div class="time-content">
+        <p class="mb10">Duration</p>
+        <el-input-number v-model="timeType.timeNum" controls-position="right" :min="1" :max="12" size="small"></el-input-number>
+        <el-select v-model="timeType.timeVal" value="1" size="small">
+          <el-option
+            v-for="item in timeType.time"
+            :key="item.id"
+            :change = "timeChange(timeType.timeVal)"
+            :value="item.value">
+          </el-option>
+        </el-select>
+        <el-time-picker
+          v-if="this.ifShowTime == 'Days'"
+          size="small"
+          format="HH:mm"
+          :picker-options="{
+            selectableRange: '18:30:00 - 23:59:00'
+          }"
+          v-model="timeType.timePicker"
+          placeholder="任意时间点">
+        </el-time-picker>
+        <el-date-picker
+          v-if="this.ifShowTime == 'weeks'"
+          size="small"
+          v-model="timeType.timeWeek"
+          type="week"
+          placeholder="选择周">
+        </el-date-picker>
+        <el-date-picker
+          v-if="this.ifShowTime == 'months'"
+          size="small"
+          v-model="timeType.timeMonths"
+          type="date"
+          :picker-options="pickerOptions"
+          placeholder="选择月">
+        </el-date-picker>
       </div>
+    </div>
   </div>
 </template>
 <script>
@@ -11,8 +48,25 @@ export default {
   name: "popupOpenTime",
   data() {
       return{
-
+        ifShowTime:"Days",
+        pickerOptions:{
+          disabledDate(time) {
+            return time.getTime() < Date.now();
+          },
+        }
       }
+  },
+  props:["timeType"],
+  methods:{
+    timeChange(val) {
+      if(val == "Days") {
+        this.ifShowTime = "Days"
+      }else if(val == "months") {
+        this.ifShowTime = "months"
+      }else if(val == "weeks") {
+        this.ifShowTime = "weeks"
+      }
+    }
   }
 };
 </script>
